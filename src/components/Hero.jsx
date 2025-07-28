d6bf687ffaa5c1fa7b85b6a2a435ea56122bca52
ankitpatil497 <ankitpatil497@gmail.com>
2025-07-28 22:30:42 +0530

    Add custom domain support

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,11 @@ import '../css/hero.css';
 const HeroSection = () => {
   return (
     <section id="hero" className="hero-section text-white text-center position-relative">
-      <img src='/banner.png' alt="Diagnostic Banner" className="img-fluid w-100 hero-banner" />
+      <img
+        src={`${process.env.PUBLIC_URL}/banner.png`}
+        alt="Diagnostic Banner"
+        className="img-fluid w-100 hero-banner"
+      />
       <div className="hero-overlay d-flex flex-column justify-content-center align-items-center">
         <h1 className="display-4 fw-bold animate-text">DR's Diagnostic Center</h1>
         <p className="lead">Accurate • Affordable • Trusted Pathology Services</p>
